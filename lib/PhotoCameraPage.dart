86fc302b25cceb864f8314428c82f2d5d922d918
Niru Fernando <119139160+alaw-bot@users.noreply.github.com>
2023-10-23 09:54:06 +0530

    created the photoCameraPage.dart

---
+++ lib/PhotoCameraPage.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class PhotoCameraPage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  PhotoCameraPage(this.cameras);
+
+  @override
+  _PhotoCameraPageState createState() => _PhotoCameraPageState();
+}
+
+class _PhotoCameraPageState extends State<PhotoCameraPage> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(widget.cameras[0], ResolutionPreset.high);
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Take Photos'),
+      ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            Navigator.pop(context,
+                image); // You can pass the captured image back to the previous page
+          } catch (e) {
+            print("Error taking a picture: $e");
+          }
+        },
+        child: Icon(Icons.camera),
+      ),
+    );
+  }
+}
